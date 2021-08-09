a096925f0bab6be183c1ee47e08f65014686d2a6
Adrian Röfer <aroefer@uni-bremen.de>
2021-08-09 20:52:44 +0200

    Commit for ws change. Attempt at splitting IKSolver from URDF but it did not quite work

--- src/kineverse_tools/ik_solver.py
+++ src/kineverse_tools/ik_solver.py
@@ -50,12 +50,8 @@ def ik_solve_one_shot(km, actuated_pose, q_now, goal_pose, visualizer=None, step
 
 
 class IKSolver(object):
-    def __init__(self, urdf_model, link_name, visualizer=None):
-        self.km = GeometryModel()
-        load_urdf(self.km, Path('robot'), urdf_model)
-
-        self.km.clean_structure()
-        self.km.dispatch_events()
+    def __init__(self, km, actuated_pose, visualizer=None):
+        self.km = km
 
         self.goal_x, self.goal_y, self.goal_z = [gm.Symbol(f'goal_{s}') for s in 'xyz']
         self.goal_qx, self.goal_qy, self.goal_qz, self.goal_qw = [gm.Symbol(f'goal_q{s}') for s in 'xyzw']
@@ -70,13 +66,13 @@ class IKSolver(object):
 
         self.goal_pose = gm.frame3_quaternion(*self.goal_vars)
 
-        eef = self.km.get_data(f'robot/links/{link_name}/pose')
+        eef = actuated_pose
 
         # Simple 9D error. Rotation will dominate, but let's see
         err_rot = gm.norm(gm.rot_of(self.goal_pose - eef).elements())
         err_lin = gm.norm(gm.pos_of(self.goal_pose - eef))
 
-        self.joint_symbols = [j.position for j in self.km.get_data('robot/joints').values() if hasattr(j, 'position') and gm.is_symbol(j.position)]
+        self.joint_symbols = [s for s in gm.free_symbols(actuated_pose) if gm.get_symbol_type(s) == gm.TYPE_POSITION]
         controlled_symbols = {gm.DiffSymbol(s) for s in self.joint_symbols}
 
         controlled_values, constraints = generate_controlled_values(self.km.get_constraints_by_symbols(controlled_symbols.union(self.joint_symbols)),
@@ -106,13 +102,7 @@ class IKSolver(object):
             (float, dict): A tuple containing the final error of the solution and
                            the final robot configuration.
         """
-        try:
-            rev_map     = {self.km.get_data(f'robot/joints/{j}').position: j for j in q_now}
-            start_state = {j: q_now[k] for j, k in rev_map.items()}
-            print(rev_map)
-        except KeyError as e:
-            raise Exception(f'Joint {k} does not seem to be a part of the robot. Original error:\n  {e}')
-        
+        start_state = q_now
         start_state.update(dict(zip(self.goal_vars, p_goal)))
 
         self.integrator = CommandIntegrator(self.qp, start_state=start_state)
@@ -120,10 +110,34 @@ class IKSolver(object):
         try:
             self.integrator.restart(title='IK integrator')
             self.integrator.run(dt=0.5, max_iterations=step_limit, logging=False)
-            return self.integrator.get_latest_error(), {rev_map[j]: v for j, v in self.integrator.state.items() 
-                                                                          if j in rev_map}
+            return self.integrator.get_latest_error(), self.integrator.state
         except Exception as e:
             traceback.print_exception(type(e), e, e.__traceback__)
             print(f'IK-Solver crashed:\n{e}')
 
         return 1.0, {}
+
+class URDF_IKSolver(IKSolver):
+    def __init__(self, urdf_model, link_name, visualizer=None):
+        km = GeometryModel()
+        load_urdf(km, Path('robot'), urdf_model)
+
+        km.clean_structure()
+        km.dispatch_events()
+
+        eef = km.get_data(f'robot/links/{link_name}/pose')
+
+        super(URDF_IKSolver, self).__init__(km, eef, visualizer)
+
+
+    def solve(self, q_now : dict, p_goal : Iterable, step_limit=50):
+        try:
+            rev_map     = {self.km.get_data(f'robot/joints/{j}').position: j for j in q_now}
+            start_state = {j: q_now[k] for j, k in rev_map.items()}
+            print(rev_map)
+        except KeyError as e:
+            raise Exception(f'Joint {k} does not seem to be a part of the robot. Original error:\n  {e}')
+
+        final_error, q_goal_state = self.solve(q_now, p_goal, step_limit) 
+
+        return final_error, {rev_map[j]: v for j, v in q_goal_state.items() if j in rev_map}
